9fba3ff834706c28fb5c8c3d42606ec543dc807e
Mike <mblack101@ufl.edu>
2019-09-29 21:38:52 -0400

    Add javascript to change background color of buttons on mouseover

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-alert('Hi');
+
+var buttonList = document.getElementsByClassName('button');
+
+for (var i = 0; i < buttonList.length; i++) {
+  buttonList[i].addEventListener("mouseover", changeBackgroundColor)
+}
+
+/* Change background color of all buttons on mouseover event */
+function changeBackgroundColor() {
+for (var i = 0; i < buttonList.length; i++) {
+  buttonList[i].style.backgroundColor = "cyan";
+}
+}
+
+
