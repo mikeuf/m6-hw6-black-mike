dbd78f84fda432e4be5bd1af2e42942f3f524a17
Mike <mblack101@ufl.edu>
2019-09-29 22:32:47 -0400

    Add message that appears when clicking Box 1

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,30 @@
 var buttonList = document.getElementsByClassName('button');
 
 for (var i = 0; i < buttonList.length; i++) {
-  buttonList[i].addEventListener("mouseover", changeBackgroundColor)
+  buttonList[i].addEventListener('mouseover', changeBackgroundColor)
 }
+
+buttonList[0].addEventListener('mousedown', printNoCigar)
 
 /* Change background color of all buttons on mouseover event */
 function changeBackgroundColor() {
-for (var i = 0; i < buttonList.length; i++) {
-  buttonList[i].style.backgroundColor = "cyan";
+  for (var i = 0; i < buttonList.length; i++) {
+    buttonList[i].style.backgroundColor = 'cyan';
+  }
 }
+
+
+function printNoCigar() {
+  var newP = document.createElement('p');
+  newP.innerHTML = 'Oooh - so close, but no cigar';
+
+  var insertionPoint = document.getElementById('messages');
+  insertionPoint.appendChild(newP);
+
 }
+
+
+
+
 
 
